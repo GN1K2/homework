dbceaa0fc34363e7d89fc6b258e999ec9e6c9bec
GN1K2 <dm6yyf@gmail.com>
2024-03-05 19:10:59 +0400

    commit

---
+++ day 10/classwork/homework.py
@@ -0,0 +1,23 @@
+#print(range(7,22))
+
+#print(range(0,11))
+
+#print(range(10,22))
+
+for i in range(1,31):
+    print(i)
+
+
+
+for q in range(1,11):
+    print( i * i )
+
+
+    result = 0
+
+    for i in range(1,11):
+        result = result + i
+
+
+
+    
