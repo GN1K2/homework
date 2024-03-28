57e634aa3ddbef4013258ee4a2c109c667768398
GN1K2 <dm6yyf@gmail.com>
2024-03-28 18:52:01 +0400

    commit

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,12 @@
+
+cars=[ "bmw" , "mazda" , "nissan" , "porshe" , "pagani" ]
+
+print (cars[3])
+
+name = ["gio" , "lomidze"]
+print (name[1] , name [0])
+
+user_age = int(input("please enter your age"))
+user_email = (input ("please enter your email"))
+user = [user_age , user_email]
+print(user)
---
+++ day 16/homework/homework.py
@@ -0,0 +1,9 @@
+#number 1
+
+family = ["gio" , "giorgi " , "natia"]
+print (family[0] ,family[1],family[2])
+
+#number 2
+ 
+number = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10"]
+print (number[0] , number[1] , number[2] , number[3] , number[4] , number[5] , number[6] , number[7] , number[8] , number[9] )
